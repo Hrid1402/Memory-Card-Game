fce284f2b903ef2e7fdc3c4e739863c9c97bc0be
Hrid1402 <Hrid1402@gmail.com>
2024-08-22 12:26:54 -0500

    feat: random pokemons displaying on click

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,38 @@ import './styles/cards.css'
 import {getPokemons} from './apiCall.js'
 import loadingICON from './assets/loadingICON.png'
 import { DifficultyButton } from './components/buttons.jsx'
+import { v4 as uuidv4 } from 'uuid';
 
 
 
+let allPokemons = [];
+let pokemonTotal = 12;
+let clickedPokemons = [];
 function App() {
-  const [allPokemons, setAllPokemons] = useState([]);
+  const [curPokemons, setCurPokemons] = useState([]);
   const [difficulty, setDifficulty] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  
 
   async function createPokemonCards(n) {
-    let newPokemons = allPokemons;
+
     for(let i = 1; i <= n; i++) {
       let pokemon = await getPokemons(i);
-      newPokemons.push(pokemon.sprites.other["official-artwork"].front_default);
-      setAllPokemons(newPokemons);
-      
+      allPokemons.push({sprite:pokemon.sprites.other["official-artwork"].front_default, name:pokemon.name});
     }
   }
-  
+
+  function setRandomPokemons(pokemonTotal, allPokemons){
+    let randomPokemons = [];
+    let copy = [...allPokemons];
+
+    for(let i = 0; i < pokemonTotal; i++){
+      let randomIndex = Math.floor(Math.random() * copy.length);
+      randomPokemons.push(copy[randomIndex]);
+      copy.splice(randomIndex, 1);
+    }
+    setCurPokemons(randomPokemons);
+  }
   
 
 
@@ -32,14 +46,35 @@ function App() {
   useEffect(() => {
     if (difficulty !== null) {
         const fetchPokemons = async () => {
-        await createPokemonCards(12);
+        await createPokemonCards(pokemonTotal);
+        setRandomPokemons(pokemonTotal, allPokemons);
         setIsLoading(false);
       };
       fetchPokemons();
     }
   }, [difficulty]);
 
-  function MainMenu({curDifficulty, allPokemons}){
+  function MainMenu({curDifficulty, curPokemons}){
+
+
+    function handleCardClick(name){
+      if(clickedPokemons.includes(name)){
+        console.log("You already clicked this card!!!!!");
+      }else{
+        console.log(`${name} clicked`);
+        clickedPokemons.push(name);
+        setRandomPokemons(pokemonTotal, allPokemons);
+      }
+      
+    }
+    function PokemonCard({name, sprite}){
+      return( 
+        <div key={uuidv4()} className='card' onClick={() => handleCardClick(name)} >
+          <img src={sprite} alt={name}/>
+        </div>
+        )
+    }
+    
     if(curDifficulty == null){
       return(
         <section>
@@ -65,13 +100,11 @@ function App() {
     }else{
       return(
         <div className='gameBlock'>
-          <h1>Pokemons left: 20</h1>
+          <h1>Pokemons left: {pokemonTotal}</h1>
           <div className='cards'>
-            {allPokemons.map((pokemon, index) => {
+            {curPokemons.map((pokemon) => {
               return(
-                <div className='card' key={index}>
-                  <img src={pokemon} alt={`Pokemon ${index}`}/>
-                </div>
+                <PokemonCard name={pokemon.name} sprite={pokemon.sprite} key={uuidv4()}/>
               )
             })}
           </div>
@@ -82,7 +115,7 @@ function App() {
 
   return (
     <>
-      <MainMenu curDifficulty={difficulty} allPokemons={allPokemons}></MainMenu>
+      <MainMenu curDifficulty={difficulty} curPokemons={curPokemons}></MainMenu>
     </>
     
     
